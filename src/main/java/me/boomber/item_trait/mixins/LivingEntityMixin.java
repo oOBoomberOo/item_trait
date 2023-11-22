08fb6a49071beb04b79a9aa9e488ddc924a8684a
oOBoomberOo <github@boomber.xyz>
2023-11-22 13:18:28 +0800

    Implemented extra attributes

--- src/main/java/me/boomber/item_trait/mixins/LivingEntityMixin.java
+++ src/main/java/me/boomber/item_trait/mixins/LivingEntityMixin.java
@@ -1,21 +1,24 @@
 package me.boomber.item_trait.mixins;
 
+import me.boomber.item_trait.attributes.ITAttributes;
 import me.boomber.item_trait.trait.Trait;
-import me.boomber.item_trait.trait.TraitRegistry;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
     @Shadow public abstract ItemStack getItemInHand(InteractionHand interactionHand);
-
-    @Shadow public abstract ItemStack getUseItem();
 
     @Inject(method = "startUsingItem", at = @At("HEAD"))
     public void startUsingItem(InteractionHand interactionHand, CallbackInfo ci) {
@@ -23,5 +26,37 @@ public abstract class LivingEntityMixin {
         var entity = (LivingEntity) (Object) this;
 
         Trait.perform(itemStack, it -> it.onStartUsing(entity, itemStack));
+    }
+
+    @Inject(method = "getDamageAfterArmorAbsorb", at = @At("RETURN"), cancellable = true)
+    public void blastResistanceAttribute(DamageSource damageSource, float f, CallbackInfoReturnable<Float> cir) {
+        if (damageSource.is(DamageTypes.EXPLOSION)) {
+            var damage = cir.getReturnValue();
+            var entity = (LivingEntity) (Object) this;
+
+            var resistance = entity.getAttributeValue(ITAttributes.BLAST_RESISTANCE);
+            var scale = 1f - (float) resistance;
+
+            cir.setReturnValue(damage * scale);
+        }
+    }
+
+    @Inject(method = "getFluidFallingAdjustedMovement", at = @At("RETURN"), cancellable = true)
+    public void sinkSpeedAttribute(double d, boolean bl, Vec3 vec3, CallbackInfoReturnable<Vec3> cir) {
+        var entity = (LivingEntity) (Object) this;
+
+        if (!entity.isNoGravity() && !entity.isSprinting()) {
+            var speed = entity.getAttributeValue(ITAttributes.SINK_SPEED);
+            var motion = cir.getReturnValue().add(0.0, -speed, 0.0);
+            cir.setReturnValue(motion);
+        }
+    }
+
+    @Inject(method = "createLivingAttributes", at = @At("RETURN"), cancellable = true)
+    private static void includeCustomAttribute(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
+        var builder = cir.getReturnValue()
+                .add(ITAttributes.SINK_SPEED)
+                .add(ITAttributes.BLAST_RESISTANCE);
+        cir.setReturnValue(builder);
     }
 }
--- src/main/java/me/boomber/item_trait/mixins/PlayerMixin.java
+++ src/main/java/me/boomber/item_trait/mixins/PlayerMixin.java
@@ -1,12 +1,15 @@
 package me.boomber.item_trait.mixins;
 
+import me.boomber.item_trait.attributes.ITAttributes;
 import me.boomber.item_trait.trait.Trait;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerMixin {
@@ -15,5 +18,13 @@ public class PlayerMixin {
         var entity = (LivingEntity) (Object) this;
         var itemStack = entity.getUseItem();
         Trait.perform(itemStack, it -> it.onBlocked(entity, itemStack));
+    }
+
+    @Inject(method = "createAttributes", at = @At("RETURN"), cancellable = true)
+    private static void includeCustomAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
+        var result = cir.getReturnValue()
+                .add(ITAttributes.BLAST_RESISTANCE)
+                .add(ITAttributes.SINK_SPEED);
+        cir.setReturnValue(result);
     }
 }
